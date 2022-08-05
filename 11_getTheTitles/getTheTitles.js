e66f9c070f3818f6f7de6ba6be733806d7e82d6a
Tennessine <aliefesobeli007@gmail.com>
2022-08-06 00:22:33 +0300

    Complete 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = [];
+    books.forEach(element => {
+        titles.push(element.title);
+    });
+    return titles;
 };
 
 // Do not edit below this line
