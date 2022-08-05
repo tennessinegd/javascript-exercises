d334a20b8670e2c1ae488bc616caf493b1620478
Tennessine <aliefesobeli007@gmail.com>
2022-08-05 23:26:37 +0300

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    sum = 0;
+    if (typeof a !== "number" || typeof b !== "number"
+        || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    const upperBound = (a > b) ? a : b;
+    const lowerBound = (upperBound === a) ? b : a;
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
