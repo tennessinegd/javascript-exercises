77c803dc192afeeb8659ffa967d78eee4d486666
Tennessine <aliefesobeli007@gmail.com>
2022-08-05 23:44:38 +0300

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9;
+    let roundedResult = Math.round(celsius * 10) / 10;
+    return roundedResult;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+    let fahrenheit = (celsius * 9 / 5) + 32;
+    roundedResult = Math.round(fahrenheit * 10) / 10;
+    return roundedResult;
 };
 
 // Do not edit below this line
