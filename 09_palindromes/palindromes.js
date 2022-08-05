470f182c2ca9a71c7a50b0023988539fc6b91236
Tennessine <aliefesobeli007@gmail.com>
2022-08-06 00:05:44 +0300

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // remove any spaces or punctuation from the string
+    let stringFormatted = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+    stringFormatted = stringFormatted.toLowerCase();
 
+    // split the string and reverse it
+    let stringReversed = stringFormatted.split("");
+    stringReversed.reverse();
+    stringReversed = stringReversed.join("");
+    
+    // compare string and its reversed version
+    if (stringFormatted === stringReversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
