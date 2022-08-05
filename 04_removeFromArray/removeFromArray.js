0bdb158e2e7da6df427739d2ebc91f2f9178e5fd
Tennessine <aliefesobeli007@gmail.com>
2022-08-05 23:14:22 +0300

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedItems) {
+    for (item of removedItems) {
+        let itemIndex = array.indexOf(item);
+        if (itemIndex === -1) continue;
+        array.splice(itemIndex, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
