f7e71274e9d4fc25cfc1e5b2cfb9788f5d941015
Tennessine <aliefesobeli007@gmail.com>
2022-08-06 00:17:02 +0300

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(order) {
 
+    order = +order;
+    
+    if (order < 0) {
+        return "OOPS";
+    }
+
+    if (order === 1 || order === 2) {
+        return 1;
+    }    
+
+    const sequence = [1, 1];
+    let result;
+
+    for (let i = 3; i <= order; i++) {
+        result = sequence.shift() + sequence[0];
+        sequence.push(result);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
