3ade7fc51da065619d3155c3afc2ee7fca375efa
Tennessine <aliefesobeli007@gmail.com>
2022-08-12 00:10:43 +0300

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = obj => (obj.reduce((prev, current) => {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    
+    let prevAge = currentYear - prev.yearOfBirth;
+    if (prev.yearOfDeath) {
+        prevAge = prev.yearOfDeath - prev.yearOfBirth 
+    }
+    
+    let currentAge = currentYear - current.yearOfBirth;
+    if (current.yearOfDeath) {
+        currentAge = current.yearOfDeath - current.yearOfBirth 
+    }
 
-};
+    return prevAge > currentAge ? prev : current;
+}));
 
 // Do not edit below this line
 module.exports = findTheOldest;
